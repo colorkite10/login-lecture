1b6d23b772cca3ed2b55f086ae7725bff56101f1
colorkite10 <colorkite10@gmail.com>
2022-05-25 15:13:30 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -6,11 +6,17 @@ const id = document.querySelector("#id"),
 
 loginBtn.addEventListener("click", login);
 
- function login()
- {
+ function login() {
      const req = {
          id: id.value,
          psword: psword.value,
      };
-     console.log(req);
+     
+     fetch("/login", {
+         method: "POST",
+         headers: {
+             "Content-Type":"application/json",
+         },
+         body: JSON.stringify(req)
+     });
  }
